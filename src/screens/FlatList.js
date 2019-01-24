3179ce97cfe7439491e6d3fabd111f0e1c2c54f3
Rahunandan K <rahu@apptrion.com>
2019-01-24 15:11:20 +0800

    Flatlist added

--- src/screens/FlatList.js
+++ src/screens/FlatList.js
@@ -9,6 +9,10 @@ import { Text, ListItem, Left, Body, Icon, Right, Title } from "native-base";
 
 
 class FlatListWithData extends Component {
+static navigationOptions = {
+  headerTitle: 'Flat List',
+}
+
 state = {
   loading: false,
   data: [],
@@ -18,28 +22,22 @@ state = {
   refreshing: false,
 };
 
-static navigationOptions = {
-    headerTitle: 'Flat List',
-  }
+componentDidMount() {
+  debugger;
+  const { page, seed } = this.state;
+  const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
 
-  componentDidMount() {
-    debugger;
-    const { page, seed } = this.state;
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-
-    fetch(url)
+  fetch(url)
     .then(res => res.json())
-    .then(res => {
+    .then((res) => {
       this.setState({
         data: page === 1 ? res.results : [...this.state.data, ...res.results],
         error: res.error || null,
         loading: false,
         refreshing: false,
-
-      },() => console.log(this.state));
+      });
     });
-
-  }
+}
 
   _renderItem = ({ item }) => {
     return (
